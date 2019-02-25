152228edc730c2bf35c5a9e370e320a6e6c2f0b3
Allan <allan.xiao@gmail.com>
2019-02-25 17:14:33 -0500

    Init

---
+++ Innouvous.Utils.Merged45/MVVM45/ViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Innouvous.Utils.Merged45.MVVM45
+{
+    public class ViewModel : Innouvous.Utils.MVVM.ViewModel
+    {
+        private DataBucket.DataBucket bucket = new DataBucket.DataBucket();
+
+        public T Get<T>([CallerMemberName] string name = "")
+        {
+            return bucket.Get<T>(name);
+        }
+
+        public void Set(object value, [CallerMemberName] string name = "")
+        {
+            bucket.Set(name, value);
+        }
+    }
+}
---
+++ innouvous.Utils.Merged/Readme.txt
@@ -0,0 +1,6 @@
+﻿<Application.Resources>
+        <ResourceDictionary Source="pack://application:,,,/Innouvous.Utils.Merged;component/GenericDictionary45.xaml"/>
+</Application.Resources>
+
+Or:
+<ResourceDictionary Source="pack://application:,,,/Innouvous.Utils.Merged;component/GenericDictionary.xaml"/>
